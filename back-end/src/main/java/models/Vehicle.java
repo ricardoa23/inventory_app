f70f1784dc545a71466783dfbf83de6ffbf2e6fe
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2024-12-25 16:07:19 -0800

    updated Vehicle model

--- back-end/src/main/java/models/Vehicle.java
+++ back-end/src/main/java/models/Vehicle.java
@@ -1,7 +1,7 @@
 package models;
 
+import enums.checkoutStatus;
 import jakarta.persistence.*;
-
 import java.util.Date;
 
 @Entity
@@ -20,6 +20,63 @@ public class Vehicle extends Item{
     @Column
     private boolean additionalTrainingRequirements;
 
+    public Vehicle() {}
 
+    public Vehicle(String name, String serial_number, checkoutStatus status, Categories categoryId, Department departmentId, String make, String model, int year, Date latestInspection, Date nextServiceInspection, boolean additionalTrainingRequirements) {
+        super(name, serial_number, status, categoryId, departmentId);
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.latestInspection = latestInspection;
+        NextServiceInspection = nextServiceInspection;
+        this.additionalTrainingRequirements = additionalTrainingRequirements;
+    }
 
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Date getLatestInspection() {
+        return latestInspection;
+    }
+
+    public void setLatestInspection(Date latestInspection) {
+        this.latestInspection = latestInspection;
+    }
+
+    public Date getNextServiceInspection() {
+        return NextServiceInspection;
+    }
+
+    public void setNextServiceInspection(Date nextServiceInspection) {
+        NextServiceInspection = nextServiceInspection;
+    }
+
+    public boolean isAdditionalTrainingRequirements() {
+        return additionalTrainingRequirements;
+    }
+
+    public void setAdditionalTrainingRequirements(boolean additionalTrainingRequirements) {
+        this.additionalTrainingRequirements = additionalTrainingRequirements;
+    }
 }
